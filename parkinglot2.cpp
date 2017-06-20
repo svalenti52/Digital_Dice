c4ac062e96bc555b8c40c2f434273d9618aa7b78
svalenti52 <svalenti52@yahoo.com>
2017-06-20 16:36:08 -0700

    Adjusting to changes in the Random_Events class.

--- parkinglot2.cpp
+++ parkinglot2.cpp
@@ -11,7 +11,7 @@
 
 int main()
 {
-    MCS_Real_and_Integer mcs_real_and_integer(1'000'000, 0.0, 1.0, 0, 9, 10,
+    MCS_Real_and_Integer mcs_real_and_integer(1'000'000, 0.0, 1.0, 0, 9, 10, 1,
             [&mcs_real_and_integer](std::vector<double>& vd) -> bool {
                 Deque_of_Difference<double> deque_of_difference(vd, 1.1, 1.1, 1.0);
 
@@ -21,6 +21,9 @@ int main()
                         (mcs_real_and_integer.random_discrete_event.events[0]);
             }, nullptr);
 
+    Distribution<int> distribution(0, 9);
+
+    Distribution<double> distribution1(0.0, 5.0);
 
     StopWatch stopWatch;
 
