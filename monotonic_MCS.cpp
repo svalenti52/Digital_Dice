577c90ab15f8c100a05c7751e06a7175cc8960cf
svalenti52 <svalenti52@yahoo.com>
2017-07-25 15:28:40 -0700

    Changed the monotonic_MCS file to use the MonteCaroSim_alpha and Distribution_alpha files.

--- monotonic_MCS.cpp
+++ monotonic_MCS.cpp
@@ -5,9 +5,12 @@
  * \brief Determine the average length of the sequence at which numbers
  * randomly chosen from the unit interval cease increasing.
  *
+ * \details Length of sequence includes the first member at which it
+ * fails to increase, i.e., the minimal sequence length is 2.
+ *
  */
 
-#include <val/montecarlo/MonteCarloSim.h>
+#include <val/montecarlo/MonteCarloSim_alpha.h>
 #include <val/montecarlo/Chronology.h>
 
 using DIST = DistributionType;
@@ -19,26 +22,31 @@ int main() {
     //-------------------------------------------------------------------------
 
     auto condition_met = [](Distribution<double, DIST::UniformReal>& number_sequence,
-            double& len_sequence) -> bool {
+            double& len_sequence, DRE& dre) -> bool {
+
 
         len_sequence = 2.0;
-        number_sequence.add_random_value_to_end(); /// sequence is now 2 in length
+        number_sequence.add_random_value_to_end(dre); /// sequence is now 2 in length
 
         while ( number_sequence.events[0] < number_sequence.events[1] ) {
             len_sequence += 1.0;
-            number_sequence.add_random_value_to_end(); /// temporarily 3 in length
+            number_sequence.add_random_value_to_end(dre); /// temporarily 3 in length
             number_sequence.events.pop_front(); /// preserve length of 2 by keeping latest
         }
 
         number_sequence.events.pop_front(); /// sequence is now 1 in length
+
         return true;
     };
 
     //-------------------------------------------------------------------------
 
-    MonteCarloSimulation_alpha<double, double, DIST::UniformReal> monteCarloSimulation(10'000'000,
-        condition_met,
-        distribution);
+    MonteCarloSimulation<double, double, DIST::UniformReal>
+            monteCarloSimulation(
+            10'000'000,
+            1,
+            condition_met,
+            distribution);
 
     StopWatch stopWatch;
 
@@ -47,4 +55,5 @@ int main() {
     stopWatch.stop();
 
     monteCarloSimulation.print_result();
+
 }
